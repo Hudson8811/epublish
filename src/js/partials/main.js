7aa0e06b66074a6905ea3e695ba9acf4531a14f7
db21 <you@example.com>
2019-11-29 14:31:03 +0300

    123

--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -2,5 +2,24 @@ $(document).ready(function() {
     $("input[name='phone']").mask(" +7 (999) 999-99-99");
 
 
+    if($('.homepage-banner-slider').length>0){
+        $('.homepage-banner-slider').slick({
+            dots: true
+        });
+    }
+
+    if($('[data-fancybox="cert"]').length>0){
+        $('[data-fancybox="cert"]').fancybox({
+            buttons: [
+                "zoom",
+                //"share",
+                //"slideShow",
+                //"fullScreen",
+                //"download",
+                //"thumbs",
+                "close"
+              ]
+        });
+    }
 
 });
