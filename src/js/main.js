801134d4fc8b5d88051c45083f49533f168548ec
PipZip <37911683+PipZip@users.noreply.github.com>
2019-12-10 17:16:33 +0600

    merge

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 /* libs */
 
-//= libs/jquery-3.3.1.min.js
-//= libs/jquery.fancybox.min.js
+// libs/jquery-3.3.1.min.js
+// libs/jquery.fancybox.min.js
 //= libs/jquery.maskedinput.min.js
 //= libs/slick.min.js
 //= libs/jquery-ui.min.js
